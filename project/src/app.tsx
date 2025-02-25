ed5b19a7945dde118bf26eb2b89c6e60751a0742
myeong-jae-hwi <audwognl@gmail.com>
2025-02-26 00:26:29 +0900

    [Feat]: supabase 연동 및 Auth 테스트 완료

--- project/src/app.tsx
+++ project/src/app.tsx
@@ -1,4 +1,25 @@
+import { useEffect } from 'react';
+import supabaseClient from './utils/SupabaseClient';
+
 function App() {
+  useEffect(() => {
+    async function Insert() {
+      const { error } = await supabaseClient
+        .from('countries')
+        .insert({ id: 1, name: 'Mordor' });
+    }
+    async function signUp() {
+      const { data, error } = await supabaseClient.auth.signUp({
+        email: 'audwognl@gmail.com',
+        password: 'password',
+      });
+      console.log(data, error);
+    }
+
+    signUp();
+    console.log();
+  });
+
   return <div className="bg-red-200">box</div>;
 }
 
--- project/src/utils/SupabaseClient.ts
+++ project/src/utils/SupabaseClient.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '../../database.types';
 
 const { VITE_SB_URL, VITE_SB_KEY } = import.meta.env;
 console.log(VITE_SB_URL, VITE_SB_KEY);
 
 // Supabase 데이터베이스와 상호 작용할 싱글 Supabase 클라이언트 생성
-const supabaseClient = createClient(VITE_SB_URL, VITE_SB_KEY);
+const supabaseClient = createClient<Database>(VITE_SB_URL, VITE_SB_KEY);
 
 export default supabaseClient;
