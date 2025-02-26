900cae93df448882dd02575c10baa13a62f9479b
myeong-jae-hwi <audwognl@gmail.com>
2025-02-26 09:36:40 +0900

    [Modify]: test 코드 주석처리

--- project/src/app.tsx
+++ project/src/app.tsx
@@ -3,21 +3,14 @@ import supabaseClient from './utils/SupabaseClient';
 
 function App() {
   useEffect(() => {
-    async function Insert() {
-      const { error } = await supabaseClient
-        .from('countries')
-        .insert({ id: 1, name: 'Mordor' });
-    }
-    async function signUp() {
-      const { data, error } = await supabaseClient.auth.signUp({
-        email: 'audwognl@gmail.com',
-        password: 'password',
-      });
-      console.log(data, error);
-    }
-
-    signUp();
-    console.log();
+    // async function signUp() {
+    //   const { data, error } = await supabaseClient.auth.signUp({
+    //     email: 'audwognl@gmail.com',
+    //     password: 'password',
+    //   });
+    //   console.log(data, error);
+    // }
+    // signUp();
   });
 
   return <div className="bg-red-200">box</div>;
