dc851e22e880f67ef3176e0aa61c6417d5f2ff0d
myeong-jae-hwi <audwognl@gmail.com>
2025-03-05 11:40:43 +0900

    [Feat]: 타로 페이지 컴포넌트 추가

---
+++ src/pages/TairoPage.tsx
@@ -0,0 +1,12 @@
+import Tairo from '@/components/Organism/Tairo';
+
+function TairoPage() {
+  return (
+    <div>
+      <h1 className="sr-only">타로보기</h1>
+      <Tairo></Tairo>
+    </div>
+  );
+}
+
+export default TairoPage;
