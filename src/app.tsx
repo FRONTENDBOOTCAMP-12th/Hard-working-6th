87214a4b6dd39f48162baed61682dda937cf2761
myeong-jae-hwi <audwognl@gmail.com>
2025-03-10 09:43:42 +0900

    [Comment]: 주석 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect } from 'react';
 import supabaseClient from './utils/SupabaseClient';
 import { BrowserRouter, Route, Routes } from 'react-router';
@@ -6,6 +5,7 @@ import TairoPage from './pages/TairoPage';
 import TairoTheme from './pages/TairoTheme';
 import PageLayout from './layout/PageLayout';
 import TairoResult from './components/Organism/TairoResult';
+import ProfileCard from './components/Organism/ProfileCard';
 
 function App() {
   // useEffect(() => {
@@ -26,6 +26,7 @@ function App() {
           <Route path="/tairo" element={<TairoPage />} />
           <Route path="/tairo-theme" element={<TairoTheme />} />
           <Route path="/tairo-result" element={<TairoResult />} />
+          <Route path="/profile-card" element={<ProfileCard />} />
         </Route>
       </Routes>
     </BrowserRouter>
