81758a800039f871b0de8c7ec35d36982092e13a
dlgudwn94 <dlgudwn82@naver.com>
2025-03-11 02:02:05 +0900

    [Feat]: 커스텀 캘린더 개발

--- src/components/Atom/Calendar.tsx
+++ src/components/Atom/Calendar.tsx
@@ -1,27 +1,32 @@
 import Calendar from 'react-calendar';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import 'react-calendar/dist/Calendar.css';
+import '../../styles/components/calendar.css';
+import moment from 'moment';
+
+type CalendarObject = HTMLDivElement & {
+  setActiveStartDate: (firstDayOfTodaysMonth: Date) => void;
+};
 
 export default function CustomCalendar() {
+  const calendarRef = useRef<null | CalendarObject>(null);
   const [date, setDate] = useState<Date | null>(new Date());
 
   const handleTodayClick = () => {
     const today = new Date();
     setDate(today);
+    const calendar = calendarRef.current;
+    calendar?.setActiveStartDate(new Date());
   };
 
   return (
     <div className="flex justify-center">
-      <div className="relative">
+      <div className="custom-calendar relative">
         <Calendar
-          onChange={(value) => setDate(value as Date)}
+          ref={calendarRef}
+          className="p-2 text-black shadow-lg rounded-lg w-[350px]"
+          onChange={(date) => setDate(date as Date)}
           value={date}
-          className="react-calendar p-4 bg-white shadow-lg rounded-lg w-[350px] md:w-[400px]"
-          tileClassName={({ view }) =>
-            `p-2 text-center rounded-md transition-all ${
-              view === 'month' ? 'hover:bg-gray-200' : 'hover:bg-blue-200'
-            }`
-          }
           navigationLabel={({ date }) => (
             <span className="text-lg font-semibold">
               {date.toLocaleString('default', {
@@ -36,11 +41,13 @@ export default function CustomCalendar() {
           next2Label={null} // ">>" 버튼(연도 이동) 제거
           prev2Label={null} // "<<" 버튼(연도 이동) 제거
           calendarType="gregory" // 일요일부터 표시되도록 그레고리력으로 변경
+          formatDay={(_locale, date) => moment(date).format('D')} // "일" 제거
         />
 
         <button
+          type="button"
           onClick={handleTodayClick}
-          className="absolute top-6 right-4 px-3 py-1 bg-blue-500 text-white text-sm rounded-lg shadow-md hover:bg-blue-600 transition"
+          className="absolute top-13 right-9 px-3 py-1 bg-[#7D3C3C] text-[#F0E6D2] border-[#5C2A2A] hover:bg-[#5C2A2A] text-sm rounded-lg shadow-md transition"
         >
           오늘
         </button>
