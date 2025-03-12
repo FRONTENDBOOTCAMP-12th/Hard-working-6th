044c056bc7901f23d268f33364e3eb925b095840
Hanna J <144082610+Hanna-Jeanne@users.noreply.github.com>
2025-03-12 10:01:23 +0900

    [Feat] 카드 스와이퍼 컴포넌트 스와이퍼 기능 구현 - 스와이퍼 스타일 css로 적용 - 애니메이션 적용 전

--- src/components/Atom/CardSlideItem.tsx
+++ src/components/Atom/CardSlideItem.tsx
@@ -1,4 +1,3 @@
-// import { tm } from '@/utils/tw-marge';
 import { useSwiperSlide } from 'swiper/react';
 
 interface Card {
@@ -10,22 +9,16 @@ interface Card {
 
 interface CardItemProps {
   card: Card;
-  isActiveProp: boolean;
 }
 
 function CardItem({ card }: CardItemProps) {
   const { isActive } = useSwiperSlide(); // SwiperSlide 내부에서 isActive 상태 가져오기
 
   return (
-    <>
-      <img
-        src={isActive ? card.src : '/src/assets/tarot-image/card-back.png'}
-        alt={isActive ? card.name : '카드 뒷면 이미지'}
-        className={
-          isActive ? 'rounded-xl translate-y-[-32px] scale-[1.15]' : ''
-        }
-      />
-    </>
+    <img
+      src={isActive ? card.src : '/src/assets/tarot-image/card-back.png'}
+      alt={isActive ? card.name : '카드 뒷면 이미지'}
+    />
   );
 }
 
--- src/components/Molecule/CardSwiper.tsx
+++ src/components/Molecule/CardSwiper.tsx
@@ -3,7 +3,7 @@ import cardsData from '@/assets/data/card_data.json';
 import CardItem from '../Atom/CardSlideItem';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay, Virtual } from 'swiper/modules';
-import { useSwiperSlide } from 'swiper/react';
+// import { useSwiperSlide } from 'swiper/react';
 
 // import 'swiper/swiper.css';
 import 'swiper/css';
@@ -25,17 +25,13 @@ interface Cards {
 const cards: Cards = cardsData;
 
 function CardSwiper() {
-  // const { isActive } = useSwiperSlide();
-
-  // console.log(isActive);
-
   return (
     <section className="flex flex-col items-center justify-center">
       <h2 className="sr-only">타로 카드 보기</h2>
 
       <Swiper
         modules={[Navigation, Autoplay, Virtual]}
-        spaceBetween={-130}
+        spaceBetween={-170}
         slidesPerView={2}
         initialSlide={1}
         centeredSlides={true}
@@ -45,50 +41,18 @@ function CardSwiper() {
         scrollbar={{ draggable: true }}
         // onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
-        className="w-[340px] h-[370px] bg-blue-950"
-        // virtual
       >
-        {cards.cards.map((card: Card, index: number) => {
+        {cards?.cards.map((card: Card, index: number) => {
           return (
-            <SwiperSlide
-              key={index}
-              className="justify-items-center h-full pt-30 relative"
-            >
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={
-                      isActive
-                        ? 'block absolute top-[2px] left-[50%] translate-x-[-50%] duration-200 ease-in-out z-20'
-                        : 'hidden'
-                    }
-                  >
-                    <div
-                      className={tm(
-                        'font-playfair text-white',
-                        'flex flex-col items-center justify-center gap-1'
-                      )}
-                    >
-                      <h3 className="text-c-xl whitespace-nowrap">
-                        {card.name}
-                      </h3>
-                      <span className="text-c-lg block">{card.number}</span>
-                    </div>
-                  </div>
+            <SwiperSlide key={index}>
+              <div className="tit">
+                <h3>{card.name}</h3>
+                <span>{card.number}</span>
+              </div>
 
-                  <CardItem card={card} isActiveProp={isActive} />
+              <CardItem card={card} />
 
-                  <p
-                    className={
-                      isActive
-                        ? 'block absolute bottom-0 left-[50%] translate-x-[-50%] duration-200 ease-in-out z-20 text-white whitespace-nowrap'
-                        : 'hidden'
-                    }
-                  >
-                    {card.mean}
-                  </p>
-                </>
-              )}
+              <p>{card.mean}</p>
             </SwiperSlide>
           );
         })}
