c1421f036ee7817218d64f7096ea536ffcef0e4a
dlgudwn94 <dlgudwn82@naver.com>
2025-03-12 17:57:25 +0900

    [Feat]: 기록 페이지 개발
    - 선택한 Chip에 따른 내용을 DB에서 불러와 출력

---
+++ src/components/Organism/History.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import CustomCalendar from '../Atom/Calendar';
+import ChipList from '../Molecule/ChipList';
+import { getMemoList } from '@/utils/supabaseHistory';
+
+interface HistoryItem {
+  id: number;
+  user_id: string;
+  created_at: string;
+  card_theme: string;
+  card_name: string;
+  content: string;
+}
+
+interface TairoHistoryProps {
+  userId: string;
+}
+
+function TairoHistory({ userId }: TairoHistoryProps) {
+  const [selectedChip, setSelectedChip] = useState<string | null>('애정운');
+  const [historyData, setHistoryData] = useState<HistoryItem[]>([]);
+  const [filteredData, setFilteredData] = useState<HistoryItem[]>([]);
+
+  const handleChipSelect = (selection: string | null) => {
+    setSelectedChip(selection);
+  };
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      const { data, error } = await getMemoList({
+        columns: '*',
+        page: 0,
+        perPage: 10,
+        orderBy: 'created_at',
+        isAscending: false,
+      });
+
+      if (data) {
+        const userHistory = data.filter(
+          (item: HistoryItem) => item.user_id === userId
+        );
+        setHistoryData(userHistory);
+      } else {
+        setHistoryData([]);
+      }
+
+      if (error) {
+        console.error('데이터 로딩 에러:', error);
+      }
+    };
+
+    if (userId) {
+      void fetchHistory();
+    }
+  }, [userId]);
+
+  useEffect(() => {
+    if (selectedChip && historyData.length > 0) {
+      const filtered = historyData.filter(
+        (item) => item.card_theme === selectedChip
+      );
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(historyData);
+    }
+  }, [selectedChip, historyData]);
+
+  return (
+    <div className="flex flex-col justify-center items-center p-6 bg-primary-gradient1">
+      <CustomCalendar />
+      <div className="mt-4">
+        <ChipList onSelect={handleChipSelect} />
+      </div>
+
+      <div className="mt-6 text-white">
+        {filteredData.length > 0 ? (
+          <ul className="mt-2">
+            {filteredData.map((item) => (
+              <li key={item.id} className="border-b border-gray-400 py-2">
+                <p>날짜: {item.created_at}</p>
+                <p>카드 이름: {item.card_name}</p>
+                <p>내용: {item.content}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>이전 기록이 없습니다.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TairoHistory;
