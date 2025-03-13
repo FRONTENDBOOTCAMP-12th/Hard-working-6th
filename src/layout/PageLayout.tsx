fc83487419823ca06c29f661fe5262e09d441bdc
dlgudwn94 <dlgudwn82@naver.com>
2025-03-13 14:06:20 +0900

    [Refactor]: 페이지별 title 구현

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -1,13 +1,32 @@
 import CommonHeader from '@/components/Atom/CommonHeader';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import CommonNav from '../components/Atom/CommonNav';
 
 //bg-[url(/src/assets/bg5.png)] bg-cover
 
-function pageLayout() {
+function PageLayout() {
+  const location = useLocation();
+
+  const getHeaderText = (pathname: string) => {
+    switch (pathname) {
+      case '/tairo-theme':
+        return '운세 선택';
+      case '/tairo-theme/tairo':
+        return '카드 뽑기';
+      case '/tairo-theme/result':
+        return '운세 결과';
+      case '/tairo-history':
+        return '기록';
+      case '/profile':
+        return '프로필';
+      default:
+        return 'title';
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-tr from-bg-gradient2 to-bg-gradient1">
-      <CommonHeader></CommonHeader>
+      <CommonHeader text={getHeaderText(location.pathname)}></CommonHeader>
       <div className="p-5 w-full h-full -z-40 pb-[110px]">
         <Outlet></Outlet>
       </div>
@@ -16,4 +35,4 @@ function pageLayout() {
   );
 }
 
-export default pageLayout;
+export default PageLayout;
