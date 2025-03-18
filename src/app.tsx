870c11184a45514f91a8171414563e525524612c
myeong-jae-hwi <audwognl@gmail.com>
2025-03-18 11:27:10 +0900

    [Test]: profiles 테이블 유틸함수 생성 및 테스트 코드 작성

--- src/app.tsx
+++ src/app.tsx
@@ -6,11 +6,11 @@ import PageLayout from './layout/PageLayout';
 import TairoResult from './components/Organism/TairoResult';
 import SignUp from './pages/SignUp';
 import HomePage from './pages/HomePage';
-// import Test from './components/Molecule/Test';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './pages/NotFound';
+import TestComponent from './components/Organism/test';
 
 function App() {
   // useEffect(() => {
@@ -42,6 +42,8 @@ function App() {
 
             <Route path="/tairo-history" element={<TairoHistory />} />
             <Route path="/profile" element={<Profile />} />
+
+            <Route path="/test" element={<TestComponent />} />
           </Route>
         </Route>
         <Route path="*" element={<NotFound />} />
