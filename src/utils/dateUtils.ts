7841a4c861ae22449d5c3dce605464f5916f4ade
dlgudwn94 <dlgudwn82@naver.com>
2025-03-19 01:57:27 +0900

    [Fix]: 기록이 존재하는 날짜 dot 표시 날짜 오차 수정 1차

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,9 +1,22 @@
 export const getMarkedDateKey = (date: string): string => {
   const dateObj = new Date(date);
 
-  // 예를 들어 KST(한국시간 UTC+9)로 변환
-  dateObj.setHours(dateObj.getHours() - 9);
+  dateObj.setHours(dateObj.getHours() + 15);
 
-  // 변환된 날짜를 "YYYY-MM-DD" 형식으로 반환
   return dateObj.toLocaleDateString('en-CA');
 };
+
+// export const getMarkedDateKey = (date: string): string => {
+//   const dateObj = new Date(date);
+
+//   // 한국 시간 (UTC+9)으로 변환
+//   const koreaOffset = 9 * 60; // KST는 UTC+9
+//   const utcOffset = dateObj.getUTCMinutes(); // UTC 기준으로 분 단위 오프셋을 가져옵니다
+//   const koreaTime = new Date(dateObj.getTime() + (koreaOffset - utcOffset) * 60000);
+
+//   // 15시간을 더합니다.
+//   koreaTime.setHours(koreaTime.getHours() + 15);
+
+//   // 'en-CA' 형식으로 출력합니다 (ISO 8601: YYYY-MM-DD)
+//   return koreaTime.toLocaleDateString('en-CA');
+// };
