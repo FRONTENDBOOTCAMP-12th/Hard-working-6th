336501ee3a04258b199d33e7e0fa7ce0a7eb872a
dlgudwn94 <dlgudwn82@naver.com>
2025-03-20 13:19:44 +0900

    [Modify]: 테스트 코드 제거

--- src/pages/TairoHistory.tsx
+++ src/pages/TairoHistory.tsx
@@ -3,21 +3,19 @@ import supabaseClient from '@/utils/SupabaseClient';
 import { useEffect, useState } from 'react';
 
 function TairoHistory() {
-  const [userId, setUserId] = useState<string | null>(
-    '4ce1ec4e-e9bb-4e95-8957-41ad0d64cd1a'
-  );
+  const [userId, setUserId] = useState<string | null>('null');
 
-  // useEffect(() => {
-  //   void (async () => {
-  //     const { data, error } = await supabaseClient.auth.getUser();
+  useEffect(() => {
+    void (async () => {
+      const { data, error } = await supabaseClient.auth.getUser();
 
-  //     if (error) {
-  //       console.error('에러 발생: ', error);
-  //     } else {
-  //       setUserId(data.user?.id);
-  //     }
-  //   })();
-  // }, []);
+      if (error) {
+        console.error('에러 발생: ', error);
+      } else {
+        setUserId(data.user?.id);
+      }
+    })();
+  }, []);
 
   return (
     <div className="min-h screen">
