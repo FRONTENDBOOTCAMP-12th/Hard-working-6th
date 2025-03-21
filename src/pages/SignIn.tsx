704084321f91d4a0393590ae43c63defbbde3853
jimyeong21 <71625640+jimyeong21@users.noreply.github.com>
2025-03-21 10:10:59 +0900

    [Chore]: 회원가입, 로그인 페이지 성능 개선
    - 이미지 포맷을 png -> svg로 변경
    - 이미지 사이즈 지정
    - 불필요한 네비게이션 바 삭제

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,18 @@
-import Logo from '/src/assets/Logo.png';
+import Logo from '/assets/logo-login.svg';
 import LoginList from '@/components/Molecule/LoginList';
 
 function SignIn() {
   return (
     <div className="flex flex-col items-center p-5 bg-light-gray h-screen">
       <h1 className="sr-only">Sign In</h1>
-      <img src={Logo} alt="tAIro" className="w-33 my-18" />
+      <img
+        src={Logo}
+        alt="tAIro"
+        className="my-20"
+        width={132}
+        height={94}
+        fetchPriority="high"
+      />
       <LoginList></LoginList>
     </div>
   );
